e8132f61a2d3c4585879749320cb7d6535a126c0
Jacob Meziere <jmeziere@pdx.edu>
2022-05-31 00:51:18 -0700

    server asks new clients for nickname and server now disconnects inactive clients

--- app/src/main/java/cs494p/irc/Client.java
+++ app/src/main/java/cs494p/irc/Client.java
@@ -3,48 +3,138 @@ package cs494p.irc;
 import java.net.*;
 import java.io.*;
 
-public class Client {
-    private static String address = "127.0.0.1";
-    private static int port = 6667;
+public class Client implements Runnable {
+    Socket socket;
+    DataInputStream input;
+    DataOutputStream output;
+    BufferedReader br;
+    public boolean isActive;
+
+    public Client(String address, int port) {
+        isActive = true;
+        try {
+            System.out.println("Connecting to server @ " + address + ":" + port);
+            this.socket = new Socket(address, port);
+            System.out.println("Fetching input stream...");
+            this.input = new DataInputStream(socket.getInputStream());
+            System.out.println("Fetching output stream...");
+            this.output = new DataOutputStream(socket.getOutputStream());
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        } catch (IOException ex) {
+            System.err.println("Failed to create Client:\n" + ex);
+        }
+    }
+
+    private void greetServer() {        
+        System.out.println("Greeting server...");
+        
+        String serverMessage = "";
+        String clientMessage = "";
+
+        try {
+            serverMessage = input.readUTF();
+            System.out.println(serverMessage);  // Server greeting, ask for nickname
+            clientMessage = br.readLine();
+            output.writeUTF(clientMessage);     // Client submit nickname
+            output.flush();
+            serverMessage = input.readUTF();    // Server asks for confirmation
+            System.out.println(serverMessage);
+            clientMessage = br.readLine();      // Client responds y/n
+            output.writeUTF(clientMessage);
+            output.flush();
+            serverMessage = input.readUTF();    // Server gives OK or asks for new nickname
+            System.out.println(serverMessage);
+
+            while (!serverMessage.startsWith("OK")) {
+                clientMessage = br.readLine();  // Client responds with new nickname
+                output.writeUTF(clientMessage);
+                output.flush();
+                serverMessage = input.readUTF();    // Server asks for confirmation
+                System.out.println(serverMessage);
+                clientMessage = br.readLine();      // Client responds y/n
+                output.writeUTF(clientMessage);
+                output.flush();
+                serverMessage = input.readUTF();    // Server gives OK or asks for new nickname
+                System.out.println(serverMessage);
+            }
+        } catch (Exception ex) {
+            System.err.println("Connection to server has been lost.");
+            disconnect();
+        }
+    }
+
+    public void disconnect() {
+        isActive = false;
+        try {
+            this.input.close();
+            this.output.close();
+            this.socket.close();
+        } catch (IOException ex) {
+            System.err.println("Failed to disconnect from server:\n" + ex);
+        }
+        System.out.println("Connection closed.");
+    }
+
+    public void run() {
+        greetServer();
+
+        Thread sendMessage = new Thread(new Runnable() {
+            public void run() {
+                while (isActive && socket.isConnected()) {
+                    try {
+                        String message = br.readLine();
+                        output.writeUTF(message);
+                        output.flush();
+                    } catch (Exception ex) {
+                        System.err.println("Connection to server has been lost.");
+                        disconnect();
+                    }
+                }
+            }
+        });
+
+        Thread receiveMessage = new Thread(new Runnable() {
+            public void run() {
+                while (isActive && socket.isConnected()) {
+                    try {
+                        String message = input.readUTF();
+                        if (!message.equals("PING")) {
+                            System.out.println(message);
+                        }
+                    } catch (Exception ex) {
+                        System.err.println("Connection to server has been lost.");
+                        disconnect();
+                    }
+                }
+            }
+        });
+
+        sendMessage.start();
+        receiveMessage.start();
+
+        while (isActive) {
+            try {
+                sendMessage.join();
+                receiveMessage.join();
+            } catch (Exception ex) {
+                System.err.println(ex);
+            }
+        }
+    }
     
     public static void main(String[] args) {
-        try {
-            Socket socket = new Socket(address, port);
-            DataInputStream input = new DataInputStream(socket.getInputStream());
-            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String address = "127.0.0.1";
+        int port = 6667;
 
-            Thread sendMessage = new Thread(new Runnable() {
-                public void run() {
-                    while (true) {
-                        try {
-                            String message = br.readLine();
-                            output.writeUTF(message);
-                        } catch (IOException ex) {
-                            System.err.println(ex);
-                        }
-                    }
-                }
-            });
-
-            Thread receiveMessage = new Thread(new Runnable() {
-                public void run() {
-                    while (true) {
-                        try {
-                            String message = input.readUTF();
-                            System.out.println(message);
-                        } catch (IOException ex) {
-                            System.err.println(ex);
-                        }
-                    }
-                }
-            });
-
-            sendMessage.start();
-            receiveMessage.start();
-
-        } catch (IOException ex) {
+        Client client = new Client(address, port);
+        Thread thread = new Thread(client);
+        thread.start();
+        try { 
+            thread.join();
+        } catch (Exception ex) {
             System.err.println(ex);
+            System.exit(1);
         }
+        System.exit(0);
     }
 }
--- app/src/main/java/cs494p/irc/Server.java
+++ app/src/main/java/cs494p/irc/Server.java
@@ -4,56 +4,177 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-public class Server {
+public class Server implements Runnable {
     static class ConnectionHandler implements Runnable{
         Socket socket;
         DataInputStream input;
         DataOutputStream output;
-        String nickname;
+        String nickname = "";
         boolean isActive = false;
     
         public ConnectionHandler(Socket socket) {
             try {
+                isActive = true;
                 this.socket = socket;
-                System.out.println("Fetching client input stream...");
+                //System.out.println("Fetching client input stream...");
                 this.input = new DataInputStream(socket.getInputStream());
-                System.out.println("Fetching client output stream...");
+                //System.out.println("Fetching client output stream...");
                 this.output = new DataOutputStream(socket.getOutputStream());
             } catch (IOException ex) {
                 System.err.println("Failed to create ConnectionHandler:\n" + ex);
             }
         }
-    
-        public void run() {
-            isActive = true;
-            System.out.println("Client " + socket + " connected.");
-            String message = "";
+
+        private void greetClient() {
+            System.out.println("Greeting client...");
+            String clientMessage = "";
             try {
-                while (isActive == true) {
-                    message = input.readUTF();
-                    System.out.println("Client " + socket + ": " + message);
+                output.writeUTF("Hello! Please enter a nickname: ");
+                boolean isCleared = false;
+                while (isCleared == false) {
+                    clientMessage = input.readUTF();
+                    nickname = clientMessage.split("\s")[0];
+                    output.writeUTF("Nickname: [" + nickname + "] Is this correct? (y/n)");
+                    clientMessage = input.readUTF();
+                    if (clientMessage.equals("y")) {
+                        output.writeUTF("OK - nickname " + nickname + " accepted!");
+                        isCleared = true;
+                        System.out.println("Client " + nickname + " connected.");
+                    } else {
+                        output.writeUTF("Please enter a nickname: ");
+                    }
                 }
             } catch (Exception ex) {
+                disconnect();
+            }
+        }
     
+        public void disconnect() {
+            System.out.println("Disconnecting client: " + nickname);
+            isActive = false;
+            try {
+                input.close();
+                output.close();
+                socket.close();
+            } catch (IOException ex) {
+                System.err.println("Failed to disconnect client:\n" + ex);
+            }
+            System.out.println("Client disconnected.");
+        }
+
+        public void run() {
+            greetClient();
+            String message = "";
+            try {
+                while (isActive == true && socket.isConnected()) {
+                    message = input.readUTF();
+                    System.out.println(nickname + ": " + message);
+                }
+            } catch (IOException ex) {
+                System.err.println("");
             }
         }
     }
 
-    private static int port = 6667;
+    static class MessageSender implements Runnable {
+        DataOutputStream dos;
+        String message;
 
-    public static void main(String[] args) {
-        Vector<ConnectionHandler> activeConnections = new Vector<>();
-        ServerSocket serverSocket = null;
+        public MessageSender(DataOutputStream dos, String message) {
+            this.dos = dos;
+            this.message = message;
+        }
 
+        public void run() {
+            try {
+                dos.writeUTF(message);
+            } catch (Exception ex) {
+                System.err.println("Failed to send message: " + message);
+            }
+        }
+    }
+
+    Vector<ConnectionHandler> activeConnections = null;
+    ServerSocket serverSocket = null;
+    BufferedReader br = null;
+    boolean isOnline = true;
+
+    public Server(int port) {
         try {
             System.out.println("Starting server...");
             serverSocket = new ServerSocket(port);
+            activeConnections = new Vector<>();
+            br = new BufferedReader(new InputStreamReader(System.in));
         } catch (IOException ex) {
             System.err.println("Failed to start server:\n" + ex);
         }
+    }
+
+    public void run() {
+        Thread reaper = new Thread(new Runnable() {
+            public void run() {
+                while (isOnline) {
+                    try {
+                        Thread.sleep(10000);
+                        System.out.println("Reaping...");
+
+                        Iterator<ConnectionHandler> i = activeConnections.iterator();
+                        while (i.hasNext()) {
+                            ConnectionHandler connection = i.next();
+                            System.out.println("reaper: checking client " + connection.nickname);
+                            try {
+                                connection.output.writeUTF("PING");
+                                connection.output.flush();
+                            } catch (Exception ex) {
+                                System.out.println("No response from client " + connection.nickname);
+                                connection.isActive = false;
+                            }
+                            if (connection.isActive == false) {
+                                connection.disconnect();
+                                i.remove();
+                            }
+                        }
+                    } catch (Exception ex) {
+                        System.err.println("reaper: " + ex);
+                    }
+                    System.out.println("Reaping complete.");
+                }
+            }
+        });
+
+        Thread console = new Thread(new Runnable() {
+            public void run() {
+                String serverCommand = "";
+                while (serverSocket.isBound()) {
+                    try {
+                        serverCommand = br.readLine();
+                        if (serverCommand.equals("ONLINE")) {
+                            for (ConnectionHandler connection : activeConnections) {
+                                System.out.println(connection.nickname + " : " + (connection.isActive ? "online":"offline"));
+                            }
+                        }
+                        if (serverCommand.equals("QUIT")) {
+                            isOnline = false;
+                            Iterator<ConnectionHandler> i = activeConnections.iterator();
+                            while (i.hasNext()) {
+                                ConnectionHandler connection = i.next();
+                                connection.output.writeUTF("Server is shutting down. Goodbye.");
+                                connection.disconnect();
+                                i.remove();
+                            }
+                        }
+                    } catch (IOException ex) {
+                        System.err.println("console: " + ex);
+                    }
+                }
+            }
+        });
+
+        reaper.start();
+        console.start();
 
         System.out.println("Server running.");
-        while (true) {
+        while (isOnline) {
             try {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client request received: " + clientSocket);
@@ -63,10 +184,16 @@ public class Server {
                 activeConnections.add(handler);
                 Thread thread = new Thread(handler);
                 thread.start();
-
             } catch (IOException ex) {
                 System.err.println(ex);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        int port = 6667;
+        Server server = new Server(port);
+        Thread thread = new Thread(server);
+        thread.start();
     }
 }
